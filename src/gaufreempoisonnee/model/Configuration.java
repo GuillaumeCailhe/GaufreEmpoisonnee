cde1c54c7d76a54880a40501fd49ccc2b795c3eb
Pepefab <34646286+Pepefab@users.noreply.github.com>
2018-04-27 17:07:14 +0200

    Create Configuration.java

---
+++ src/gaufreempoisonnee/model/Configuration.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gaufreempoisonnee.model;
+
+/**
+ *
+ * @author lefebfab
+ */
+public class Configuration {
+    private Case[][] plateau;
+    
+    public Configuration(Case[][] p){
+        plateau = p;
+    }
+    
+    public Configuration(){
+        
+    }
+}
