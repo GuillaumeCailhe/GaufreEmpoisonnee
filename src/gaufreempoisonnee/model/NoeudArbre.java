f776987cef2e7e99f3a88621aae1b9afcfce9d27
Pepefab <34646286+Pepefab@users.noreply.github.com>
2018-04-30 16:52:39 +0200

    IA : modif pour fonctionner avec Arbre et/ou

--- src/gaufreempoisonnee/model/NoeudArbre.java
+++ src/gaufreempoisonnee/model/NoeudArbre.java
@@ -6,22 +6,77 @@
 package gaufreempoisonnee.model;
 
 import java.util.ArrayList;
-import java.util.List;
+import java.lang.StringBuilder;
 
 /**
  *
  * @author lefebfab
  */
-public class NoeudArbre {
-    private Configuration configuration; 
-    private List<NoeudArbre> fils;
+public class NoeudArbre{
     
-    public NoeudArbre(Configuration c){
-        configuration = c;
+    private int yJoue;
+    private int xJoue;
+           
+    // EVALUATION DU NOEUD
+    private boolean valeur;
+    
+    private NoeudArbre pere;
+    private ArrayList<NoeudArbre> fils;
+    
+    public NoeudArbre(int y, int x, NoeudArbre p){
+        yJoue = y;
+        xJoue = x;
+        valeur = false;
         fils = new ArrayList<NoeudArbre>();
+        pere = p;
     }
-    
-    public void AjouterFils(NoeudArbre f){
+
+    public int getyJoue() {
+        return yJoue;
+    }
+
+    public int getxJoue() {
+        return xJoue;
+    }
+
+    public NoeudArbre getPere() {
+        return pere;
+    }
+
+    public boolean getValeur() {
+        return valeur;
+    }
+
+    public void setValeur(boolean valeur) {
+        this.valeur = valeur;
+    }
+
+    public void ajouterFils(NoeudArbre f){
         fils.add(f);
     }
+
+    public ArrayList<NoeudArbre> getFils() {
+        return fils;
+    }
+
+    @Override
+    public String toString() {
+
+        StringBuilder sj = new StringBuilder();{
+            NoeudArbre pere = this.getPere();
+            while(pere != null){
+                sj.append("-");
+                pere = pere.getPere();
+            }
+            
+            sj.append("Coup: " + yJoue + ";" + xJoue + "; valeur: " + valeur + "\n");
+
+            for (NoeudArbre f : fils) {
+                sj.append(f.toString());
+            }
+
+            return sj.toString();
+        }
+    }
+    
 }
