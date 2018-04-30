34012bebe51f45fda21f35efb30c9a6e86f3bd45
Pepefab <34646286+Pepefab@users.noreply.github.com>
2018-04-30 16:54:01 +0200

    IA : début IA difficile

--- src/gaufreempoisonnee/model/JoueurIADifficile.java
+++ src/gaufreempoisonnee/model/JoueurIADifficile.java
@@ -11,12 +11,31 @@ package gaufreempoisonnee.model;
  */
 public class JoueurIADifficile extends Joueur{
 
+    NoeudArbre minMax;
+    
     public JoueurIADifficile(int idJoueur, Plateau plateau) {
         super(idJoueur, plateau);
+       
     }
 
     public boolean jouerCoup() {
-        return jouerCoupPrecis(0,0);
+        int y, x;
+        
+        if(plateau.getCase(2,0) == Case.VIDE && plateau.getCase(0, 2) == Case.VIDE){ // Si le terrain est suffisament petit pour le min max
+            if(minMax == null){
+                minMax = new NoeudArbre(plateau.getHauteur(), plateau.getLargeur(), null);
+                plateau.arbreMinMax1(minMax);
+            } 
+            y = 0;
+            x = 0;
+        } else { // terrain encore trop grand, l'IA va chercher à le réduire
+            y = 0;
+            x = 0;
+        }
+
+        
+        
+        return jouerCoupPrecis(y,x);
     }
     
 }
