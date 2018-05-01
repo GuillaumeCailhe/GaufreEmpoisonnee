cc394f533f2000f878c1df6871862a70b51f2aac
Pepefab <34646286+Pepefab@users.noreply.github.com>
2018-05-01 15:00:10 +0200

    IA : première version fonctionnelle de la difficile

--- src/gaufreempoisonnee/model/JoueurIADifficile.java
+++ src/gaufreempoisonnee/model/JoueurIADifficile.java
@@ -5,37 +5,81 @@
  */
 package gaufreempoisonnee.model;
 
+import java.util.ArrayList;
+import java.util.Hashtable;
+import java.util.Random;
+
 /**
  *
  * @author lefebfab
  */
 public class JoueurIADifficile extends Joueur{
 
-    NoeudArbre minMax;
+    private NoeudArbre minMax;
+    private boolean modeMinMax;
     
     public JoueurIADifficile(int idJoueur, Plateau plateau) {
         super(idJoueur, plateau);
-       
+        minMax = new NoeudArbre(plateau.getHauteur(), plateau.getLargeur(), null);
+        modeMinMax = false;
     }
 
     public boolean jouerCoup() {
         int y, x;
+        Random r = new Random();
         
-        if(plateau.getCase(2,0) == Case.VIDE && plateau.getCase(0, 2) == Case.VIDE){ // Si le terrain est suffisament petit pour le min max
-            if(minMax == null){
-                minMax = new NoeudArbre(plateau.getHauteur(), plateau.getLargeur(), null);
+        if(plateau.getCase(3,3) == Case.VIDE){ // Si le terrain est suffisament petit pour le min max
+            if(!modeMinMax){
+                modeMinMax = true;
                 plateau.arbreMinMax1(minMax);
             } 
-            y = 0;
-            x = 0;
-        } else { // terrain encore trop grand, l'IA va chercher à le réduire
-            y = 0;
-            x = 0;
-        }
+            Hashtable<Boolean, ArrayList<NoeudArbre>> m = minMax.getFilsVictorieux();
+            if(m.get(true).size() > 0 || m.get(false).size() > 0){
+                if(m.get(true).size() > 0) { // On peut entrer dans une configuration gagnante ! Victoire :D
+                    System.out.println("je gagne !");
+                    int i = r.nextInt(m.get(true).size());
+                    y = m.get(true).get(i).getyJoue();
+                    x = m.get(true).get(i).getxJoue();
+                } else { // meh. pas gagnant mais pas perdant non plus
+                    System.out.println("Je peut gagner !");
+                    int i = r.nextInt(m.get(false).size());
+                    y = m.get(false).get(i).getyJoue();
+                    x = m.get(false).get(i).getxJoue();
+                }
 
+            } else { // OH MON DIEU JE VAIS MOURIR, JE VAIS DEVOIR MANGER DU POISON :'(
+                System.out.println("Je vais perdre :c");
+                System.out.println(minMax);
+                int i = r.nextInt(minMax.getFils().size());
+                y = minMax.getFils().get(i).getyJoue();
+                x = minMax.getFils().get(i).getxJoue();
+            }    
+        } else { // terrain enc1ore trop grand, l'IA va chercher à le réduire
+            do {
+                y = r.nextInt(plateau.getHauteur()-3)+3;
+                x = r.nextInt(plateau.getLargeur()-3)+3;
+            } while(plateau.getCase(y,x) == Case.VIDE);
+        }
         
+        System.out.println("Je joue en : " + y + "-" + x);
+        ajouterCoup(y,x);
         
+        NoeudArbre fils = new NoeudArbre(y,x,minMax);
+        minMax.ajouterFils(fils);
         return jouerCoupPrecis(y,x);
     }
     
+    public void ajouterCoup(int y, int x){
+        if(modeMinMax){
+            for(NoeudArbre f : minMax.getFils()){
+                if(f.getyJoue() == y && f.getxJoue() == x){
+                    minMax = f;
+                }
+            }
+        } else {
+            NoeudArbre f = new NoeudArbre(y,x,minMax);
+            minMax.ajouterFils(f);
+            minMax = f;
+        }        
+    }
 }
