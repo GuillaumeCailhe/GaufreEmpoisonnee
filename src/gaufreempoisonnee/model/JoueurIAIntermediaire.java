b63a87b49568683d7749ae23bde7d4bafbb62613
Pepefab <34646286+Pepefab@users.noreply.github.com>
2018-05-01 22:43:32 +0200

    Bugfix taille plateau

--- src/gaufreempoisonnee/model/JoueurIAIntermediaire.java
+++ src/gaufreempoisonnee/model/JoueurIAIntermediaire.java
@@ -28,8 +28,7 @@ public class JoueurIAIntermediaire extends Joueur{
         int y, x;
         Random r = new Random();
         
-        if(plateau.getCase(3,4) == Case.VIDE /* && (plateau.getCase(0,4) == Case.VIDE || plateau.getCase(4,0) == Case.VIDE)*/){ // Si le terrain est suffisament petit pour le min max
-            System.out.println("test");
+        if(plateau.getCase(3,4) == Case.VIDE  && (plateau.getCase(0,4) == Case.VIDE || plateau.getCase(3,0) == Case.VIDE)){ // Si le terrain est suffisament petit pour le min max
             if(!modeMinMax){
                 modeMinMax = true;
                 plateau.arbreMinMax1(minMax);
