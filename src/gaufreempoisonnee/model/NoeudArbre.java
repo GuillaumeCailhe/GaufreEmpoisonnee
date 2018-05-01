31a2bca7fc43bbd0a422347b038fc3d895c32573
Pepefab <34646286+Pepefab@users.noreply.github.com>
2018-05-01 15:00:43 +0200

    IA : bugfix pour l'arbre et/ou

--- src/gaufreempoisonnee/model/NoeudArbre.java
+++ src/gaufreempoisonnee/model/NoeudArbre.java
@@ -7,6 +7,8 @@ package gaufreempoisonnee.model;
 
 import java.util.ArrayList;
 import java.lang.StringBuilder;
+import java.util.Dictionary;
+import java.util.Hashtable;
 
 /**
  *
@@ -58,7 +60,24 @@ public class NoeudArbre{
     public ArrayList<NoeudArbre> getFils() {
         return fils;
     }
+    
+    public Hashtable<Boolean, ArrayList<NoeudArbre>> getFilsVictorieux(){
+        Hashtable<Boolean, ArrayList<NoeudArbre>> m = new Hashtable<Boolean,ArrayList<NoeudArbre>>();
+        m.put(true, new ArrayList<NoeudArbre>());
+        m.put(false, new ArrayList<NoeudArbre>());
 
+        for(NoeudArbre f : fils){
+            if(f.valeur){
+                m.get(true).add(f);
+            }
+            if(f.getFilsVictorieux().get(true).size() > 0){ // On prend si au moins un fils mène à une configuration gagnante
+                m.get(false).add(f);
+            }
+        }
+        return m;
+    }
+    
+    
     @Override
     public String toString() {
 
